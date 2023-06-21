3e941eebe3335fd70ae5047cf4ae034990d52db4
ToiznerD <toiznerd@gmail.com>
2023-06-22 00:43:28 +0300

    implement contact

--- components/home.jsx
+++ components/home.jsx
@@ -2,12 +2,16 @@ import { useContext} from "react"
 import Context from "../utils/context"
 import Layout from "./layout"
 import { styles } from '../utils/style'
+import Image from 'next/image'
+import hamburger from '../assets/burger-menu.png'
+import table from '../assets/seatwise.png'
 export default function Home() {
     const { state, dispatch } = useContext(Context)
 
     return(
       <Layout title="Event Management">
-          <div className={styles.Title}>SeatWise</div>
+         <Image src={table} width={300} height={150} className="mx-auto"/>
+        {/*<div className={styles.Title}>SeatWise</div>*/}
           <div className={styles.content}>
           Use a wedding seating tool to plan where your guests are seated. Super easy! 
         </div>
