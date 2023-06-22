f8b956f10f2f06cdaae06e1ed5c7fb3758d9ad7d
raz228822 <raz228822@gmail.com>
2023-06-22 15:25:36 +0300

    Adding assiging guests to table functionallity

--- components/manage_table_seats.jsx
+++ components/manage_table_seats.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import Context from "../utils/context";
 import Layout from "./layout";
 import { styles } from '../utils/style';
@@ -12,69 +12,84 @@ export default function ManageTableSeats() {
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'))
         async function fetchData() {
-          const response = await fetch('/api/guests', {
+        const response = await fetch('/api/guests', {
             method: "POST",
             headers: {
-              "Content-Type": "application/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({ userId: user.userId })
-          })
-    
+            })
+
           if (response.ok) {
             let res = await response.json();
             setGuests(res)
           }
         }
-        fetchData()
-    }, [])
-
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('user'))
+        
         async function fetchDataTables() {
-          const response = await fetch('/api/tables', {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ userId: user.userId })
-          })
-    
-          if (response.ok) {
-            let res = await response.json();
-            setTables(res)
-          }
-        }
+            const response = await fetch('/api/tables', {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ userId: user.userId })
+              })
+        
+              if (response.ok) {
+                let res = await response.json();
+                setTables(res)
+              }
+            }
+
+        fetchData()
         fetchDataTables()
     }, [])
 
     const handleRowClick = (rowId) => {
         if(rowId === selectedRow)
-            setSelectedRow(0)
+            setSelectedRow(null)
         else
             setSelectedRow(rowId);
         console.log(rowId)
     };
 
+    const addGuest = (index) => {
+        if(tables[index].current_seats === tables[index].max_seats) {
+            console.log("Table is full")
+            return
+        }
+
+        if(selectedRow !== null && !tables[index].guests.includes(selectedRow)) {
+            tables[index].guests.push(selectedRow)
+            tables[index].current_seats += 1
+            guests.guests[selectedRow].table = index
+            setSelectedRow(null)
+            console.log(tables[index].guests)
+        }
+    }
+
+   
+
     return (
-        <Layout w="100vw">
+        <Layout w="95vw">
             <div className="flex justify-between relative">
                 <div className="w-screen">
                 {Array.isArray(tables) && tables.map((table, index) => {
                     if(table === null)
                         return null
                     return(
-                        <RoundTableComponent key={index} current_seats={table.current_seats} guests_in_table={table.guests_in_table} max_seats={table.max_seats} subject={table.subject} x={table.x} y={table.y} />
+                        <RoundTableComponent addGuest={() => addGuest(index)} key={index} current_seats={table.current_seats} guests_in_table={table.guests_in_table} max_seats={table.max_seats} subject={table.subject}  />
                     )
                 })}
                 </div>
-                <div className=" w-[500px] h-[full] overflow-y-auto">
+                <div className=" w-[550px] h-[full] overflow-y-auto">
                     <table className="w-full border-gray-500 border-4">
                         <thead>
                             <tr>
-                                <th className="py-2">Name</th>
-                                <th className="py-2">Amount</th>
-                                <th className="py-2">Category</th>
-                                <th className="py-2">Table</th>
+                                <th className={styles.td}>Name</th>
+                                <th className={styles.td}>Amount</th>
+                                <th className={styles.td}>Category</th>
+                                <th className={styles.td} >Table</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -82,11 +97,11 @@ export default function ManageTableSeats() {
                         if (guest === null)
                             return null
                         return(
-                            <tr key={index} onClick={() => handleRowClick(index)} className={selectedRow === index ? styles.selectedRow : styles.normalRow}>
+                            <tr key={index} onClick={() => handleRowClick(index)} className={selectedRow === index ? styles.selectedRow : guest.table !== 0 ? styles.normalRowDisabled : styles.normalRow}>
                                 <td className={styles.td}>{guest.name}</td>
                                 <td className={styles.td}>{guest.amount}</td>
                                 <td className={styles.td}>{guest.category}</td>
-                                <td className={styles.td}>0</td>
+                                <td className={styles.td}>{guest.table}</td>
                             </tr>
                             )
                         })}
--- components/roundTableComponent.jsx
+++ components/roundTableComponent.jsx
@@ -1,23 +1,20 @@
-import { useRef } from "react";
 import  roundTable  from '../assets/roundTable.png';
 import Image from 'next/image';
 import Draggable from 'react-draggable';
 
 export default function roundTableComponent (props) {
-    const { current_seats, guests_in_table, max_seats, subject, x, y} = props
-    const refImage = useRef(null)
-    const divRef = useRef(null)
+    const { current_seats, guests_in_table, max_seats, subject, x, y, addGuest} = props
 
     const handleMouseDown = (event) => {
       event.preventDefault()
+      addGuest()
     };
-
 
     return (
       <div className="h-full">
-        <Draggable ref={divRef} onMouseDown={handleMouseDown} bounds="parent">
+        <Draggable onMouseDown={handleMouseDown} bounds="parent">
             <div className="cursor-pointer absolute flex flex-col items-center justify-center font-bold my-1" style={{ left: `${x}px`, top: `${y}px` }}>
-                <Image src={roundTable} ref={refImage} alt="pic" style={{ width: '100px', height: '100px' }}/>
+                <Image src={roundTable} alt="pic" style={{ width: '100px', height: '100px' }}/>
                 <div className="absolute mb-4">
                   {current_seats}/{max_seats}
                 </div>
